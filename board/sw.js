669a014d62ffc086773d921d46312a6ab12ddebd
Carter Hammond <carterh@trueline.net.au>
2026-08-18 00:39:48 +1000

    chore: phone board 2026-08-18

--- board/sw.js
+++ board/sw.js
@@ -1,4 +1,4 @@
-const CACHE='board-vc642c687';
+const CACHE='board-ve7784058';
 const ASSETS=['manifest.json','icon-192.png','icon-512.png','icon-180.png'];
 
 self.addEventListener('install', e => {
